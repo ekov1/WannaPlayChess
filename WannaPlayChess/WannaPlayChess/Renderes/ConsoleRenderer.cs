e862283acba3a6613bffc47a60247c699763d2dc
ekov1 <kristian_ekov@abv.bg>
2018-02-15 19:49:16 +0200

    render board

--- WannaPlayChess/WannaPlayChess/Renderes/ConsoleRenderer.cs
+++ WannaPlayChess/WannaPlayChess/Renderes/ConsoleRenderer.cs
@@ -10,6 +10,8 @@ namespace WannaPlayChess.Renderes
     {
         private const string Logo = "WannaPlayChess";
         private const int CharactersPerBoardSquare = 9;
+        private const ConsoleColor black = ConsoleColor.DarkGray;
+        private const ConsoleColor white = ConsoleColor.Gray;
 
         public void RenderMainMenu()
         {
@@ -22,11 +24,44 @@ namespace WannaPlayChess.Renderes
 
         public void RenderBoard(IBoard board)
         {
+            Console.Clear();
             var startRowPrintInt = Console.WindowHeight / 2 - (board.TotalRows / 2) * CharactersPerBoardSquare;
             var startColPrintInt = Console.WindowWidth / 2 - (board.TotalCols / 2) * CharactersPerBoardSquare;
 
-            Console.SetCursorPosition(startColPrintInt, startRowPrintInt);
-            Console.WriteLine("x");
+            var currentRowPrint = startRowPrintInt;
+            var currentColPrint = startColPrintInt;
+
+            int counter = 0;
+            for (int top = 0; top < board.TotalRows; top++)
+            {
+                for (int left = 0; left < board.TotalCols; left++)
+                {
+                    currentColPrint = startColPrintInt + top * CharactersPerBoardSquare;
+                    currentRowPrint = startRowPrintInt + left * CharactersPerBoardSquare;
+
+                    if (counter % 2 == 0)
+                    {
+                        Console.BackgroundColor = black;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = white;
+                    }
+
+                    for (int i = 0; i < CharactersPerBoardSquare; i++)
+                    {
+
+                        for (int j = 0; j < CharactersPerBoardSquare; j++)
+                        {
+                            Console.SetCursorPosition(currentColPrint + j, currentRowPrint + i);
+                            Console.WriteLine(" ");
+                        }
+                    }
+                    counter++;
+                }
+                counter++;
+            }
+
             Console.ReadLine();
         }
     }
